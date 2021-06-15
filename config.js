871bddf1c57127658b2946c0fe9f9e6cf63159b6
Glitch (shorthaired-hollow-muskmelon) <none>
2021-06-15 09:14:27 +0000

    🦃🍞 Checkpoint ./config.js:26684847/201

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ module.exports = {
 	bot: {
 		token: 'ODUzNjU3OTI1NDQ3NzEyNzg4.YMYk5Q.UQJvKGo9b_hk7jacZO9L8J2pVaU',
 		prefix: '!',
-		clientSecret: 'QHAOxBahPoIGJlL6Q-RUTjVF02rCnsiH',
+		clientSecret: 't35yZ9VVumE_fNFJZapsBhAvE-FdB6yc',
 		clientID: '853657925447712788'
 	},
 	website: {
 		protocol: 'https://',
-		domain: 'shorthaired-hollow-muskmelon.glitch.me',
+		domain: 'mencomencoba-ni.glitch.me',
 		port: 8080,
 		captcha: {
 			sitekey: 'SITE_KEY',
