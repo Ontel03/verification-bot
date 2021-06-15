ebe46fdf6707e0fc0f090285af1639447dc48ea1
Glitch (shorthaired-hollow-muskmelon) <none>
2021-06-15 08:44:13 +0000

    🏔🍁 Checkpoint ./package.json:26684847/22 ./config.js:26684847/145 ./dashboard/templates/index.ejs:26684847/17

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	bot: {
-		token: 'B0T.T0K3N.H3R3',
+		token: 'ODUzNjU3OTI1NDQ3NzEyNzg4.YMYk5Q.UQJvKGo9b_hk7jacZO9L8J2pVaU',
 		prefix: '!',
-		clientSecret: 'CLIENT_SECRET',
-		clientID: 'CLIENT_ID'
+		clientSecret: 'QHAOxBahPoIGJlL6Q-RUTjVF02rCnsiH',
+		clientID: '853657925447712788'
 	},
 	website: {
 		protocol: 'https://',
